f03ca2b2c10953e61655f229ae56506d17512883
Sérgio Dornelas <sergiodornelasdev@gmail.com>
2023-04-05 16:53:13 -0300

    endereço

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -50,3 +50,17 @@ Cypress.Commands.add('loginVazioEsenhaCorreta', (senha) => {
     cy.get('.woocommerce-form > .button').click()
 })
 
+Cypress.Commands.add('endereco', (nome, sobrenome, instituicao, pais, endereco, cidade, estado, cep ) => {
+    cy.get('.woocommerce-MyAccount-navigation-link--edit-address > a').click()
+    cy.get(':nth-child(2) > .title > .edit').click()
+    cy.get('#shipping_first_name').clear().type(nome)
+    cy.get('#shipping_last_name').clear().type(sobrenome)
+    cy.get('#shipping_company').clear().type(instituicao)
+    cy.get('#select2-shipping_country-container').click().type(pais).get('[aria-selected="true"]').click()
+    cy.get('#shipping_address_1').clear().type(endereco)
+    cy.get('#shipping_city').clear().type(cidade)
+    cy.get('#select2-shipping_state-container').click().type(estado).get('[class="select2-results__options"]').click()
+    cy.get('#shipping_postcode').clear().type(cep)
+    cy.get('.button').click()
+})
+
